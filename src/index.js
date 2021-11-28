e5bc25aa00a6158c6c8e6cd7574068a6f886dae8
Dmitriy Ovsienko <ovsienkodima@mail.ru>
2021-11-29 00:16:08 +0300

    new theme

--- src/index.js
+++ src/index.js
@@ -1,63 +1,68 @@
 // index.js
 // This is the main entry point of our application
 /**
+ * переключится на node js 12v nvs use 12
  * для запуска mongosh запускаем cmd от имени админа пишем команду mongosh
  * запускаем mongod на диске С в папке MongoDB (не обязательно)
  * запускаем web/ npm run dev
  * запускаем сервер из папки /api, npm start для запросов
- * Глава 15. Аутентификация и состояние Создание формы авторизации 185 стр
+ * Глава 15. Аутентификация и состояние Защищенные маршруты 190 стр
  */
 
-const express = require('express')
-const helmet = require('helmet')
-const {ApolloServer} = require('apollo-server-express')
-require('dotenv').config()
-const jwt = require('jsonwebtoken')
+const express = require('express');
+const helmet = require('helmet');
+const { ApolloServer } = require('apollo-server-express');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const getUser = token => {
   if (token) {
     try {
-      return jwt.verify(token, process.env.JWT_SECRET) // если токен годный возвращаем информацию пользователя из токена
+      return jwt.verify(token, process.env.JWT_SECRET); // если токен годный возвращаем информацию пользователя из токена
     } catch (err) {
-      new Error('Session invalid')
+      new Error('Session invalid');
     }
   }
-}
+};
 
 /**импортируем локальные модули */
-const db = require('./db') //вызов подключения к БД
-const typeDefs = require('./schema')
-const resolvers = require('./resolvers') //CRUD создавать считывать обновлять удалять общий шаблон с методами
-const modeles = require('./modeles/index')
-const depthLimit = require('graphql-depth-limit')
-const { createComplexityLimitRule } = require('graphql-validation-complexity')
+const db = require('./db'); //вызов подключения к БД
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers'); //CRUD создавать считывать обновлять удалять общий шаблон с методами
+const modeles = require('./modeles/index');
+const depthLimit = require('graphql-depth-limit');
+const { createComplexityLimitRule } = require('graphql-validation-complexity');
 
-const port = process.env.PORT || 4000 //запускаем сервер на порте указанном в .env файле или на порте 4000
-const DB_HOST = process.env.DB_HOST //Переменная хоста 
+const port = process.env.PORT || 4000; //запускаем сервер на порте указанном в .env файле или на порте 4000
+const DB_HOST = process.env.DB_HOST; //Переменная хоста
 
-const app = express()
-app.use(helmet())
+const app = express();
+app.use(helmet());
 
-db.connect(DB_HOST)//Подключаем БД
+db.connect(DB_HOST); //Подключаем БД
 
 //настройка Apollo Server
-const server = new ApolloServer({ 
-  typeDefs, 
+const server = new ApolloServer({
+  typeDefs,
   resolvers,
-  validationRules: [depthLimit(5), createComplexityLimitRule(1000)], 
+  validationRules: [depthLimit(5), createComplexityLimitRule(1000)],
   context: ({ req }) => {
-    const token = req.headers.authorization //получаем токен пользователя из заголовка
-    const user = getUser(token) //пытаемся извлечь пользователя с помощью токена
-    console.log(user)
-    return { modeles, user } //возвращаем модель и пользователя
-    } 
-  })
+    const token = req.headers.authorization; //получаем токен пользователя из заголовка
+    const user = getUser(token); //пытаемся извлечь пользователя с помощью токена
+    console.log(user);
+    return { modeles, user }; //возвращаем модель и пользователя
+  }
+});
 
 //применяем промежуточное ПО Apollo GraphQl и указываем путь к /api
-server.applyMiddleware({ app, path: '/api' })
+server.applyMiddleware({ app, path: '/api' });
 
-app.get('/', (req,res) => res.send('hello web server!!!!!'))
-app.listen({ port }, () => console.log(`GraphQl Server running at http://localhost:${port}${server.graphqlPath}`))
+app.get('/', (req, res) => res.send('hello web server!!!!!'));
+app.listen({ port }, () =>
+  console.log(
+    `GraphQl Server running at http://localhost:${port}${server.graphqlPath}`
+  )
+);
 
 /* запрос через GraphQl для получения массива данных
 query {
